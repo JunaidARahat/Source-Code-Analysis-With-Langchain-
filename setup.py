9f2f3679d682088d5510f86ae520d847b7c016c7
JunaidARahat <mjunaidrahat1990@gmail.com>
2024-06-22 20:36:57 +0500

    setup.py and requirements added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'JunaidARahat',
+    author_email= 'mjunaidrahat1990@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
